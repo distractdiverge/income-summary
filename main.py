3ff04b200928437d5027ac5873becd42d6576faa
Allie Lapinski <alex.lapinski@gmail.com>
2025-02-05 12:01:36 -0500

    Added shell of script

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import pdfplumber
+import re
+import datetime
+import json
+from collections import defaultdict
+
+def extract_transactions_from_pdf(pdf_path):
+    pass
+
+def parse_transactions(text):
+    pass
+
+def aggregate_income_by_month(transactions):
+    pass
+
+def process_statements(directory):
+    pass
+
+if __name__ == "__main__":
+    statements_directory = "<PATH>"
+    process_statements(statements_directory)
