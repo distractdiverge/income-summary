20fbba091aa1e15242e02b400a12ca2bcda0206d
Allie Lapinski <alex.lapinski@gmail.com>
2025-02-08 16:02:09 -0500

    Fixed unit test to test 1 valid 1 empty

--- test_parse_transaction.py
+++ test_parse_transaction.py
@@ -4,10 +4,20 @@ from main import parse_transaction
 @pytest.mark.parametrize(
     ("input", "expected"),
     [
-        ("test", None),
+        ("01/17 30.00 XXXX Debit Card Purchase Paypal *Add To Bal", 
+            ("01/17", "30.00", "XXXX Debit Card Purchase Paypal *Add To Bal")),
         ("test2", None),
     ],
 )
 def test_parse_transaction(input, expected):
-    output = parse_transaction(input)
-    assert output == expected
+    actual = parse_transaction(input)
+
+    if expected is None:
+        assert actual is None
+    else:
+        date, amount, description = actual
+        
+        expected_date, expected_amount, expected_description = expected
+        assert date == expected_date
+        assert amount == expected_amount
+        assert description == expected_description
