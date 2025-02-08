81b2bb15dbbcd87145c8d8f669ab76803a7878d5
Allie Lapinski <alex.lapinski@gmail.com>
2025-02-08 13:00:17 -0500

    extracted out parsing a single txn

--- main.py
+++ main.py
@@ -17,11 +17,10 @@ def extract_transactions_from_pdf(file_path):
 
     return transactions
 
-def parse_transactions(text):
-    transactions = []
-    lines = text.split("\n")
-    print(len(lines), " Lines")
-
+def parse_transaction(text):
+    ###
+    ### Parse a SINGLE transaction and return its parts
+    ###
     # Possible 'TXN Type' Categories (extracted from examples)
     # "XXXX Debit Card Purchase", 
     # "POS Purchase",
@@ -32,26 +31,34 @@ def parse_transactions(text):
 
     # Example Line to match: 01/17 30.00 XXXX Debit Card Purchase Paypal *Add To Bal
     transaction_pattern = re.compile(r"(\d{2}/\d{2})\s+([\d,]+\.\d{2})\s+(.+)")
+    match = transaction_pattern.search(text.strip())
+
+    if match:
+        date_str, amount, description = match.groups()
+        date_str, amount, description = match.groups()
+            
+        # date_obj = datetime.datetime.strptime(f"{date_str}/{cuurent_year}")
+        # date_obj = datetime.datetime.now()
+
+        # amount_value = float(amount.replace(",",""))
+
+        return (date_str, amount, description)
+    
+    return None
+
+
+
+def parse_transactions(text):
+    transactions = []
+    lines = text.split("\n")
+    print(len(lines), " Lines")
 
     for line in lines:
-        match = transaction_pattern.search(line.strip())
-        if "deposit" in line.lower():
-            print(f"{repr(line)}")
+        transaction = parse_transaction(line)
         
-        if match:
-            #print("matched")
-            date_str, amount, description = match.groups()
-            
-            # date_obj = datetime.datetime.strptime(f"{date_str}/{cuurent_year}")
-            date_obj = datetime.datetime.now()
-
-            amount_value = float(amount.replace(",",""))
-
-            #print(f"Processing TXN: {date_str}, {amount}, {description}")
-
-            #if any(keyword in description.lower() for keyword in ["deposit", "ach credit", "direct deposit"]):
-            if amount_value > float(1000.00):
-                transactions.append((date_str, description, amount_value))
+        if transaction is not None:
+            (date_str, amount_value, description) = transaction
+            transactions.append((date_str, description, amount_value))
             
     return transactions
 
